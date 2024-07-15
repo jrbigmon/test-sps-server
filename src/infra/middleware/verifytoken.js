14e0c2006a594d4b79bcee8ae62541ab75994a6b
jrbigmon <juniorterriaga0@gmail.com>
2024-07-15 12:26:28 -0300

    feat: authenticated users

--- src/infra/middleware/verifytoken.js
+++ src/infra/middleware/verifytoken.js
@@ -4,10 +4,13 @@ const verifyToken = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
 
-    jwt.verify(token, "shiu");
+    console.log(token);
+
+    jwt.verify(token.trim(), "shiu");
 
     return next();
   } catch (err) {
+    console.log(err);
     return res.status(401).json({
       error: "User not authenticated",
       statusCode: 401,
