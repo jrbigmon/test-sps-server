ce9db7de8a549f5c84b8ea895875aae12e2ddfc0
jrbigmon <juniorterriaga0@gmail.com>
2024-07-15 15:25:00 -0300

    fix: added params in delete router

--- src/infra/users/routes/userRoutes.js
+++ src/infra/users/routes/userRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get("/users", verifyToken, UserService.findAll);
 router.post("/users", verifyToken, UserService.create);
-router.delete("/users", verifyToken, UserService.delete);
+router.delete("/users/:id", verifyToken, UserService.delete);
 router.get("/users/:id", verifyToken, UserService.findById);
 router.put("/users/:id", verifyToken, UserService.update);
 router.post("/login", UserService.login);
